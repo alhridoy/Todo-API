54116d6724e2ae79e58096310916dfc4f9cb7177
Al-Iqram Elahee Hridoy <aliqramalaheehridoy@gmail.com>
2018-06-19 22:26:29 +0600

    TestPOST/todosroute

--- server/test/server.test.js
+++ server/test/server.test.js
@@ -5,7 +5,7 @@ const {Todo} = require('./../models/todo');
 
 //Empty databse so that i assumed that dabase is empty
 
-
+//testing life-cycle method,here done aync func will run after testing everything and it will remove the database
 beforeEach((done)=>{
 
     Todo.remove({}).then(()=> done());
@@ -18,7 +18,7 @@ describe('POST/todos', ()=>{
 
    request(app)
    .post('/todos')
-   .send({text})
+   .send({text})//add the text to the databse
    .expect(200)
    .expect((res)=>{
 
@@ -39,4 +39,27 @@ describe('POST/todos', ()=>{
        }).catch((e)=> done(e));
    })
  });
+ 
+
+ it('should not create todo without invalid data', (done)=>{
+ 
+
+    request(app)
+    .post('/todos')
+    .send({})
+    .expect(400)
+    .end((err,res)=>{
+     if(err){
+
+        return done(err);
+     }
+   //to search in databse
+     Todo.find().then((todos)=>{
+     expect(todos.length).toBe(0)
+     done();
+     }).catch((e)=> done(e));
+    })
+
+ })
+
 });

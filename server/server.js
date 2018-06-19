fbd32e10022929a723fd35b789ce1cdc5a75f2db
Al-Iqram Elahee Hridoy <aliqramalaheehridoy@gmail.com>
2018-06-19 17:09:28 +0600

    'AddGET/todosroute'

--- server/server.js
+++ server/server.js
@@ -22,8 +22,20 @@ var todo = new Todo({
   })
 });
 
+//another route 
+
+app.get('/todos', (req,res)=>{
+
+    Todo.find().then((todos)=>{
+   res.send({todos})
+    },(e)=>{
+        res.status(400).send(e);
+    })
+})
 
 
 app.listen(3000,()=>{
     console.log('Started on port 3000')
 });
+
+module.exports ={app};
